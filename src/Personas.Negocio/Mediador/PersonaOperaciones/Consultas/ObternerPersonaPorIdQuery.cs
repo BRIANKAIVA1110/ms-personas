a2d4b571fad7a6a1fde46cba1e35a7dd90ca8b04
brian villarroel <villarroelbrian1110@gmail.com>
2021-03-12 14:39:31 -0300

    prueba fluentValidation

--- src/Personas.Negocio/Mediador/PersonaOperaciones/Consultas/ObternerPersonaPorIdQuery.cs
+++ src/Personas.Negocio/Mediador/PersonaOperaciones/Consultas/ObternerPersonaPorIdQuery.cs
@@ -10,18 +10,13 @@ namespace Personas.Negocio.Mediador.PersonaOperaciones.Consultas
     public class ObternerPersonaPorIdQuery: IRequest<PersonaDTO>
     {
         public int Id { get; set; }
-
-        public ObternerPersonaPorIdQuery(int Id)
-        {
-            this.Id = Id;
-        }
     }
 
     public class ObternerPersonaPorIdValidator: AbstractValidator<ObternerPersonaPorIdQuery>
     {
         public ObternerPersonaPorIdValidator() 
         {
-            RuleFor(x => x.Id).NotNull().WithMessage("El campo Id es requerido");
+            RuleFor(x => x.Id > 0).NotNull().WithMessage("Requerido");
         }
     }
 }
